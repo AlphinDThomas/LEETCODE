f52219db6894bdb154c98d9e91ff1232848db4a1
AlphinDThomas <alphinprofessional23@gmail.com>
2026-06-18 23:00:39 +0530

    Valid Pallindrome

---
+++ Problem125.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        substring=""
+        s=s.lower()
+        for i in s:
+            if i.isalnum():
+                substring +=i
+        return substring == substring[::-1]
+        
